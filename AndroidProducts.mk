d74d37d18ab5172d3ffcfd78dfd11206a14efd12
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-26 13:13:29 +0100

    judyln: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: lge Model: LM-G710 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_judyln.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_judyln-user \
+    omni_judyln-userdebug \
+    omni_judyln-eng
